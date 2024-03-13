54b0dac8be056031e4fa8aa8a5f2a4e5ba542a69
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-03-13 23:43:08 +0530

    Message Detail Page Implementation

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,13 +1,19 @@
 const asyncHandler = require("express-async-handler");
 
+const Message = require("../models/message");
+
 // Message List
 exports.message_list = asyncHandler(async (req, res, next) => {
+
+    
     res.send("NOT IMPLEMENTED: Message List");
 });
 
 // Message Detail
 exports.message_detail = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Message Detail - " + req.params.id);
+
+    const message = await Message.findById(req.params.id).populate("creator").exec();
+    res.render("message_detail", { message: message });
 });
 
 // Message Create GET
